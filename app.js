775ec8af907b0907d895f37df54736d823092704
lecharles <charleslozano@gmail.com>
2025-12-02 18:26:18 -0800

    Ex1

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Testing that everything is working!");
+console.log("And it's also working on port 5500!");
+
+/*
+Exercise 1: Define an empty array
+1) Create an empty array and assign it to a variable called `foods`.
+Exercise 1 has been completed for you:
+Thanks for this!
+*/
+
+const foods = [];
+console.log('Exercise 1 result:', foods);
+
+
+
